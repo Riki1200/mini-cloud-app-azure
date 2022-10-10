860bdfbcee262b6613fdd5e23f843140e6506ac9
Romeo <45272844+Riki1200@users.noreply.github.com>
2022-10-10 15:52:47 -0400

    feat: modules added

--- webappcloud-server/src/main.ts
+++ webappcloud-server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
+  app.flushLogs();
+  app.enableVersioning();
+  app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();
